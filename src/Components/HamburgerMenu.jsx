50bd60ac74a2203578ddb06e942d79dfc079b034
Allison Livingston <livingstona16@gmail.com>
2024-12-08 11:37:11 -0700

    Fixed icon xmlns and increased opacity for hamburger menu background

--- src/Components/HamburgerMenu.jsx
+++ src/Components/HamburgerMenu.jsx
@@ -27,7 +27,7 @@ const styles = {
         width: "100%",
         height: "100%",
         backgroundColor: "whitesmoke",
-        opacity: 0.15,
+        opacity: 0.5,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
